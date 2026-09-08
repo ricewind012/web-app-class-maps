e5626b2b1aee39abaf530b501cdd80bd60dbbb99
ricewind012 <58827198+ricewind012@users.noreply.github.com>
2026-09-08 20:23:16 +0000

    object classes

--- scripts/ssr_test/parse-ssr-module.ts
+++ scripts/ssr_test/parse-ssr-module.ts
@@ -1,6 +1,8 @@
 import { readFile } from "node:fs/promises";
-import { parse, Visitor } from "oxc-parser";
+import { parse, Visitor, type Expression, type PropertyKey } from "oxc-parser";
 import * as ReactUtils from "./react-utils";
+
+type ValveComponent = "steamavatar";
 
 const [, , file] = process.argv;
 const text = await readFile(file, "utf8");
@@ -15,14 +17,47 @@ if (errors.length > 0) {
 	process.exit(1);
 }
 
-// TODO: detect component
+const componentPropMap: Record<ValveComponent, Record<string, string>> = {
+	steamavatar: {
+		// skip rgSources & playerLinkDetails & bLimitProfileFrameAnimationTime
+		Avatar: "avatarURL",
+		AvatarFrame: "bDisableAnimation",
+	},
+};
+
+const componentPropSkipMap: Record<ValveComponent, Set<string>> = {
+	steamavatar: new Set([
+		"rgSources",
+		"playerLinkDetails",
+		"bLimitProfileFrameAnimationTime",
+	]),
+};
+
+const components = new Map<ValveComponent, Map<string, string>>();
 const varNameToClassName = new Map<string, string>();
-const visitations: string[] = [];
+
+let functionDepth = 0;
 
 const visitor = new Visitor({
 	FunctionDeclaration(decl) {
+		// For module-context global detection
+		functionDepth++;
+
 		if (!ReactUtils.isComponent(decl)) {
 			return;
+		}
+
+		// TODO: detect component file
+		const component: ValveComponent = "steamavatar";
+		const props = ReactUtils.getComponentProps(decl);
+		if (decl.params.length === 1 && props) {
+			const names = [...props.keys()];
+			for (const name of names) {
+				if (componentPropSkipMap[component].has(name)) {
+					console.error(`Skipping '${name}'`);
+					return;
+				}
+			}
 		}
 
 		// Never undefined (see above), satisfy TypeScript
@@ -37,11 +72,82 @@ const visitor = new Visitor({
 			return;
 		}
 
-		const { start, end } = arg;
-		const outer = text.slice(start, end);
+		//
+	},
+	"FunctionDeclaration:exit"() {
+		functionDepth--;
+	},
+	ObjectExpression(decl) {
+		// Only globals (by module context)
+		if (functionDepth !== 0) {
+			return;
+		}
 
-		const props = ReactUtils.getComponentProps(decl);
-		console.log("%o\n%o\n---------------", props, outer);
+		const { start, end } = decl;
+		const outer = text.slice(start, end);
+		// Empty object
+		if (end - start === 2) {
+			return;
+		}
+
+		const kv: [PropertyKey, Expression][] = [];
+		for (const prop of decl.properties) {
+			if (prop.type !== "Property") {
+				continue;
+			}
+
+			const k = prop.key;
+			// Most likely React props
+			// TODO: test in future, happened one time only?
+			if (k.type === "Identifier" && k.name === "className") {
+				return;
+			}
+
+			const v = prop.value;
+			// Most likely the object that's declared before the class names
+			// TODO: test in future
+			if (v.type === "ArrowFunctionExpression") {
+				return;
+			}
+
+			kv.push([k, v]);
+		}
+
+		console.log(
+			"%sInput: %s%s\n",
+			Bun.color("gray", "ansi"),
+			Bun.color("white", "ansi"),
+			outer,
+		);
+
+		const pairs: [string, string][] = [];
+		for (const [k, v] of kv) {
+			if (k.type === "Literal" && v.type === "TemplateLiteral") {
+				pairs.push([String(k.value), v.quasis[0].value.raw]);
+			} else if (k.type === "Identifier" && v.type === "Identifier") {
+				const realClassName = varNameToClassName.get(v.name);
+				if (!realClassName) {
+					console.error("No class name for '%s'", k.name);
+				}
+
+				pairs.push([k.name, realClassName ?? v.name]);
+			}
+		}
+
+		const keyWidth = Math.max(...pairs.map(([k]) => k.length));
+		const valueWidth = Math.max(...pairs.map(([, v]) => v.length));
+		for (const [k, v] of pairs) {
+			console.log(
+				"%s%s %s-> %s%s",
+				Bun.color("white", "ansi"),
+				k.padEnd(keyWidth),
+				Bun.color("gray", "ansi"),
+				Bun.color("white", "ansi"),
+				v.padEnd(valueWidth),
+			);
+		}
+
+		console.log("%s------------------", Bun.color("rgb(64 64 64)", "ansi"));
 	},
 	VariableDeclarator(decl) {
 		if (!decl.init) {
@@ -49,30 +155,32 @@ const visitor = new Visitor({
 		}
 
 		// TODO: sometimes they are in an object, like the old webpack
-		// modules
+		// modules BUT some are still in dedi vars
 		if (decl.id.type !== "Identifier") {
 			return;
 		}
 
-		const { start, end } = decl.init;
-		const value = text.slice(start, end);
-
-		// Data URIs
-		if (value.slice(1, 6) === "data:") {
+		// SSR strings are always templates without variables
+		if (decl.init.type !== "TemplateLiteral" || decl.init.quasis.length > 1) {
 			return;
 		}
 
-		// Usually SSR classes are 12 (+2 for quotes) characters long
+		const value = decl.init.quasis[0].value.raw;
+		// Data URIs
+		if (value.slice(0, 5) === "data:") {
+			return;
+		}
+
+		// SSR classes are 12 characters long, with an underscore if starting
+		// with a number
 		const len = value.length;
-		if (len !== 14) {
-			//console.error("len !== 14, maybe wrong?", { len, value });
+		if (len !== 12 && len !== 13) {
 			return;
 		}
 
 		const { name } = decl.id;
 		varNameToClassName.set(name, value);
-		visitations.push(`${name} = ${value}`);
 	},
 });
 visitor.visit(program);
-//console.log({ varNameToClassName });
+console.log({ varNameToClassName });
